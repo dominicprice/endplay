7d78d0ae96d873becaf18607767f738a2366befc
Dominic Price <dominicprice@outlook.com>
2021-12-12 23:39:08 +0000

    Fix setup.cfg and bump version

--- src/endplay/utils/__init__.py
+++ src/endplay/utils/__init__.py
@@ -1,3 +1,5 @@
 """
-Miscellaneous routines. 
+Utility functions which either don't fit neatly into
+another package, or are used by many different packages
+and are placed here for common access.
 """
