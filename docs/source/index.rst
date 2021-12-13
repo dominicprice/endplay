bd1ffb68180895aafacb8c063abbd79ee2417721
Dominic Price <dominicprice@outlook.com>
2021-12-13 00:07:35 +0000

    Fix documentation and import of libc on windows

--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,1 +1,19 @@
-..include: pages/index.rst
+endplay documentation
+=====================
+
+These are the documentation pages for the Python package endplay. Most of 
+the information can also be found in the README.md file in the github repo,
+but is presented here broken down into sections along with some extra information
+pages and a comprehensive API reference of all the modules and packages the library
+exposes.
+
+.. toctree::
+	:maxdepth: 3
+	:caption: Main Contents:
+
+	pages/readme/01_intro
+	pages/readme/02_building_and_installing
+	pages/readme/03_overview_of_submodules
+	pages/readme/04_tutorial
+	pages/inputformat
+	pages/reference/endplay
--- docs/source/modules/generate_index.py
+++ docs/source/modules/generate_index.py
@@ -4,23 +4,25 @@ from itertools import takewhile
 def setup(app):
 	app.connect("builder-inited", builder_inited)
 	app.add_config_value("index_pages_root", None, 'env', [Path])
+	app.add_config_value("index_output_file", None, 'env', [Path])
 	app.add_config_value("index_sections", None, 'env', [list])
 	app.add_config_value("index_template_file", None, 'env', [Path])
 
 def builder_inited(app):
 	pages_dir = app.config.index_pages_root
+	root_dir = app.config.index_output_file.parent
 	# Accumulate the sections
 	sections = []
 	for section in app.config.index_sections:
 		path = Path(pages_dir, section)
 		if path.is_file():
-			sections.append(str(path.with_suffix("").relative_to(pages_dir)))
+			sections.append(str(path.with_suffix("").relative_to(root_dir)))
 		else:
-			subsections = [str(file.with_suffix("").relative_to(pages_dir)) for file in path.iterdir()]
+			subsections = [str(file.with_suffix("").relative_to(root_dir)) for file in path.iterdir()]
 			sections += sorted(subsections)
 
 	# tee input to output, replacing the %SECTIONS% tag with the actual sections
-	infile, outfile = app.config.index_template_file, Path(pages_dir, "index.rst")
+	infile, outfile = app.config.index_template_file, app.config.index_output_file
 	with open(infile) as fin, open(outfile, "w") as fout:
 		for line in fin:
 			if line.strip() == "%SECTIONS%":
