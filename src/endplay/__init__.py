095abc56bb0670f75188be4968e87178f037f870
Dominic Price <dominicprice@hotmail.co.uk>
2021-12-29 18:10:27 +0000

    Add __buildtime__ with timestamp to config file

--- src/endplay/__init__.py
+++ src/endplay/__init__.py
@@ -10,4 +10,6 @@ from endplay.interact import *
 from endplay.parsers import *
 from endplay.evaluate import *
 from endplay.types import *
-from endplay.config import __author__, __version__, __version_info__
+from endplay.config import \
+	__version__, __version_info__, __author__, \
+	__buildtime__, suppress_unicode
