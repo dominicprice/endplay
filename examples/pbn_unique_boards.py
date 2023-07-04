aab0bada07810ad4287e8129a195de27a15ee880
Dominic Price <dominicprice@outlook.com>
2023-07-04 12:12:23 +0100

    remove unfinished example

--- examples/pbn_unique_boards.py
+++
@@ -1,9 +0,0 @@
-import endplay.parsers.pbn as pbn
-from pathlib import Path
-
-if __name__ == "__main__":
-	pbn_file = Path("pbn_files", "example1.pbn")
-	with open(pbn_file) as f:
-		boards = pbn.load(f)
-
-	deals = 
