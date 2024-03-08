7a9d8f312c4b82f5e0f1de4a90be40bb55fa031e
Dominic Price <dominicprice@outlook.com>
2024-03-08 10:05:02 +0000

    update version script

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -60,8 +60,8 @@ VERSION="${PARTS[0]}.${PARTS[1]}.${PARTS[2]}"
 (poetry version "$VERSION")
 
 # update config.py
-sed -i "s/__version__ = .*/__version__ = \"${ENDPLAY_VERSION}\"/g" 'src/endplay/config.py'
-sed -i "s/__version_info__ = .*/__version__ = (${PARTS[0]}, ${PARTS[1]}, ${PARTS[2]})/g" 'src/endplay/config.py'
+sed -i "s/__version__ = .*/__version__ = \"${PARTS[0]}.${PARTS[1]}.${PARTS[2]}\"/g" 'src/endplay/config.py'
+sed -i "s/__version_info__ = .*/__version_info__ = (${PARTS[0]}, ${PARTS[1]}, ${PARTS[2]})/g" 'src/endplay/config.py'
 
 # commit version and create git tag
 git add pyproject.toml
